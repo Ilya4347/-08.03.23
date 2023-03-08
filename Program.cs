7d1e234b8f34e0b647f6c982e37525dfa57112fc
Ilya <ilya.solomennikov@gmail.com>
2023-03-08 18:53:00 +0400

    решил задачу номер 6

--- Program.cs
+++ Program.cs
@@ -31,3 +31,14 @@ if(c>max) max = c;
     Console.Write("max = ");
     Console.WriteLine(max);*/
    
+
+/*Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка(number%2==0)).
+
+4 -> да
+-3 -> нет
+7 -> нет*/
+
+Console.WriteLine("Введите число ");
+int number = int.Parse(Console.ReadLine()!); 
+if(number%2==0) Console.WriteLine("число является четным");
+ else Console.WriteLine("число является нечетным");   
