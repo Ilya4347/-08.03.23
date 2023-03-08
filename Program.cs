9d0b6ec37d5d16998c01d924a116b6ae002a6137
Ilya <ilya.solomennikov@gmail.com>
2023-03-08 19:35:12 +0400

    решил задачу номер 8

--- Program.cs
+++ Program.cs
@@ -38,7 +38,20 @@ if(c>max) max = c;
 -3 -> нет
 7 -> нет*/
 
-Console.WriteLine("Введите число ");
+/*Console.WriteLine("Введите число ");
 int number = int.Parse(Console.ReadLine()!); 
 if(number%2==0) Console.WriteLine("число является четным");
- else Console.WriteLine("число является нечетным");   
+ else Console.WriteLine("число является нечетным");*/
+
+
+ /*Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+5 -> 2, 4
+8 -> 2, 4, 6, 8*/   
+
+Console.WriteLine("Введите число ");
+int n = int.Parse(Console.ReadLine()!); 
+int a = 1;
+while (a<=n){
+    if(a%2==0) Console.WriteLine(a + "");
+    a++;}
