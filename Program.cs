90243afa10408c61330118bb6a5ee804492e8b28
Ilya <ilya.solomennikov@gmail.com>
2023-03-12 17:36:17 +0400

    решил задачу номер 10

--- Program.cs
+++ Program.cs
@@ -49,9 +49,25 @@ if(number%2==0) Console.WriteLine("число является четным");
 5 -> 2, 4
 8 -> 2, 4, 6, 8*/   
 
-Console.WriteLine("Введите число ");
+/*Console.WriteLine("Введите число ");
 int n = int.Parse(Console.ReadLine()!); 
 int a = 1;
 while (a<=n){
     if(a%2==0) Console.WriteLine(a + "");
     a++;}
+*/ 
+
+/*Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1 */
+
+Console.WriteLine("Введите число ");
+string number = Console.ReadLine()!;
+if (number.Length == 3){
+Console.WriteLine ($"{number}={number[1]}");
+}
+else {
+Console.WriteLine ("Введено неверное число");
+}
