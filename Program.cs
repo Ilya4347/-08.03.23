72d947c9dd460a68108b9febfd769abd5528d5c3
Ilya <ilya.solomennikov@gmail.com>
2023-03-16 20:28:34 +0400

    решил задачу номер 23

--- Program.cs
+++ Program.cs
@@ -112,7 +112,7 @@ A (3,6,8); B (2,1,-7), -> 15.84
 
 A (7,-5, 0); B (1,-1,9) -> 11.53*/
 
-int ReadInt(string text)
+/*int ReadInt(string text)
 {
 System.Console.Write(text);
 return Convert.ToInt32(Console.ReadLine());
@@ -126,4 +126,28 @@ b[0] = ReadInt("Введите координату X точки B: ");
 b[1] = ReadInt("Введите координату Y точки B: ");
 b[2] = ReadInt("Введите координату Z точки B: ");
 double res = Math.Round(Math.Sqrt(Math.Pow(a[0]-b[0], 2) + Math.Pow(a[1]-b[1], 2) + Math.Pow(a[2]-b[2], 2)), 2);
-System.Console.WriteLine("Расстояние между точками = " + res);
+System.Console.WriteLine("Расстояние между точками = " + res);*/
+
+/*Задача 23
+
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125*/
+
+int ReadInt(string text)
+{
+System.Console.Write(text);
+return Convert.ToInt32(Console.ReadLine());
+}
+
+void NumberPow(int number)
+{
+for (int i = 1; i <= number; i++)
+{
+System.Console.Write(Math.Pow(i, 3) + " ");
+}
+}
+
+int number = ReadInt("Введите число: ");
+NumberPow(number);
