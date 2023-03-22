854060a838bc514ef2e09e5f94e8d05c412ffea7
Ilya <ilya.solomennikov@gmail.com>
2023-03-22 21:21:51 +0400

    решил задачу номер 29

--- Program.cs
+++ Program.cs
@@ -213,7 +213,7 @@ int Pow (int num, int rank)
 
 9012 -> 12
 */
-Console.Write("Введите число: ");
+/*Console.Write("Введите число: ");
 int num = int.Parse (Console.ReadLine()!);
 Console.WriteLine($"Сумма цифр ={GetSumNums(num)}");
 int GetSumNums (int number)
@@ -227,4 +227,19 @@ int GetSumNums (int number)
     return sum;
     
     
+}
+*/
+/*Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. (числа берете любые)*/
+
+int [] array = GetRandomArray (8,0,100);
+Console.WriteLine ($"[{String.Join(",",array)}]");
+
+int[]GetRandomArray(int size, int minValue, int maxValue)
+{
+int[]result = new int [size];
+for (int i=0; i<size; i++)
+{
+result[i]= new Random().Next(minValue, maxValue);
+}
+return result;
 }
