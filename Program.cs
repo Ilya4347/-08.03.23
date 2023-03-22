43f51e5db328cf078ea7353fdc985c338cc0d845
Ilya <ilya.solomennikov@gmail.com>
2023-03-22 20:57:08 +0400

    решил задачу номер 27

--- Program.cs
+++ Program.cs
@@ -187,7 +187,7 @@ NumberPow(number);*/
 2, 4 -> 16
 */
 
-Console.Write("Введите число: ");
+/*Console.Write("Введите число: ");
 int A = int.Parse (Console.ReadLine()!);
 Console.Write("Введите степень: ");
 int B = int.Parse (Console.ReadLine()!);
@@ -202,4 +202,29 @@ int Pow (int num, int rank)
     }
     return result;
 
+}
+*/
+
+/*Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+452 -> 11
+
+82 -> 10
+
+9012 -> 12
+*/
+Console.Write("Введите число: ");
+int num = int.Parse (Console.ReadLine()!);
+Console.WriteLine($"Сумма цифр ={GetSumNums(num)}");
+int GetSumNums (int number)
+{
+    int sum = 0;
+    while (number>0)
+    {
+        sum+=number%10;
+        number/=10;
+    }
+    return sum;
+    
+    
 }
