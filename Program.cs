d9573fd33ee01b9ce6321964c9f26de0332b269e
Ilya <ilya.solomennikov@gmail.com>
2023-03-22 20:20:05 +0400

    решил задачу номер 25

--- Program.cs
+++ Program.cs
@@ -180,3 +180,26 @@ int number = ReadInt("Введите число: ");
 NumberPow(number);*/
 
 
+/*Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B. (Задачи, решенные через Math.Pow, не будут считаться правильными, так как задача стоит в том, чтобы написать цикл)
+
+3, 5 -> 243 (3⁵)
+
+2, 4 -> 16
+*/
+
+Console.Write("Введите число: ");
+int A = int.Parse (Console.ReadLine()!);
+Console.Write("Введите степень: ");
+int B = int.Parse (Console.ReadLine()!);
+Console.WriteLine ($"{A} в степени {B}={Pow(A,B)}");
+int Pow (int num, int rank)
+{
+    if (B==0) return 1;
+    int result = num;
+    for (int i = 2; i<=rank; i++)
+    {
+        result*=num;
+    }
+    return result;
+
+}
